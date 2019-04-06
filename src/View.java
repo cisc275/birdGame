32f5056b73b92dce04ef4fbb6db1ce2e8579e1fc
Zachary Carey <zacharyc@udel.edu>
2019-04-06 15:48:04 -0400

    added moveables updated some stubs

--- src/View.java
+++ src/View.java
@@ -2,6 +2,7 @@ import java.io.*;
 import javax.imageio.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.*;
 
 
@@ -17,14 +18,23 @@ class View extends JPanel{
 	void buildFrame() {}
 	public void paint(Graphics g) {}
 	void addControllerToMouse(MouseMotionListener m) {}
-	void update(EatingBird b, List<Food> f) {}
-	void update(MigratingBird b, List<Enemy> e, List<Gust> g) {}
-	void update(BreedingBird b, List<Predator> p, Nest n) {}
+	void update(EatingBird b, List food) {}
+	void update(MigratingBird b, List enemies, List gusts) {}
+	void update(BreedingBird b, List predators, Nest n) {}
 	JPanel generateQuiz() {
 		JPanel panel = new JPanel();
 		return panel;
 	}
 	void displayScore(int eScore, int mScore, int bScore) {}
+	
+	
+	public void addControllerToKeyboard(Controller controller) {
+		// TODO Auto-generated method stub
+	}
+	public void addControllerToButton(Controller controller) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 }
